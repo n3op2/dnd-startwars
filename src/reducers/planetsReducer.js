e6b07c262cea9c2f9fe45b74f94cd62e6a5f1492
n3op2 <paulius.michelevicius@gmail.com>
2018-09-23 02:12:56 +0100

    Rename...

--- src/reducers/planetsReducer.js
+++ src/reducers/planetsReducer.js
@@ -7,8 +7,8 @@ const planetsReducer = (state = reduxStore.planets, { type, payload }) => {
   switch (type) {
     case 'UPDATE_ELEMENT':
       const planet = filterById(state, payload.id);;
-      const index = [...state].findIndex(item => item === planet[0]);
-      const newState = [...state];
+      const index = state.findIndex(item => item === planet[0]);
+      const newState = state;
       newState[index].interceptor = payload.obj;
       return newState; 
     default:
