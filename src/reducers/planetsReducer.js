8d3cddcc00c9a302f3c96d0352fb20a049b2fb9e
n3op2 <paulius.michelevicius@gmail.com>
2018-09-23 02:11:03 +0100

    Added filterById function.

--- src/reducers/planetsReducer.js
+++ src/reducers/planetsReducer.js
@@ -1,10 +1,13 @@
 import { reduxStore } from '../Store.js';
 
+//move to something like utils.js?
+const filterById = (arr, id) => arr.filter(item => item.id === id);
+
 const planetsReducer = (state = reduxStore.planets, { type, payload }) => {
   switch (type) {
     case 'UPDATE_ELEMENT':
-      const getPlanet = [...state].filter(item => item.id === payload.id);
-      const index = [...state].findIndex(item => item === getPlanet[0]);
+      const planet = filterById(state, payload.id);;
+      const index = [...state].findIndex(item => item === planet[0]);
       const newState = [...state];
       newState[index].interceptor = payload.obj;
       return newState; 
